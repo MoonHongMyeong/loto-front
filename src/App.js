f4bdad935ff9e02201fd22547280894465200d3a
MoonHongMyeong <moonhongmyeong@gmail.com>
2023-12-14 17:21:33 +0900

    Add : App.js에 리엑트 라우터 적용 [#1/#2]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Main from './components/pages/Main';
+import SignUp from './components/pages/SignUp';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <React.Fragment>
-      <Main></Main>
-    </React.Fragment>
+    <BrowserRouter>  
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/signUp' element={<SignUp/>}/>       
+      </Routes>
+    </BrowserRouter>
   );
 }
 
