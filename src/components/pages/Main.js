e4e2e98e9b536e1f0a98d71f883d7e9e00d904d6
MoonHongMyeong <moonhongmyeong@gmail.com>
2023-12-14 17:23:39 +0900

    Add : 메인에서 유저 정보가 없으면 로그인 모달을 띄우도록 구현 [#2]

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import styled from "styled-components";
 import Header from "../layout/Header";
 import Section from "../layout/Section";
 import Footer from "../layout/Footer";
+import { UserContext } from "../../contexts/UserContext";
+import LoginModal from "../modal/LoginModal";
 
 const Main = () => {
+    const userInfo = useContext(UserContext);
+    
     return (
         <>
             <Header/>
-            <Section/>
+            <Section>
+                <Container>
+
+                </Container>
+            </Section>
             <Footer/>
+            {userInfo.user.nickname === "" && <LoginModal/>}
         </>
     )
 }
+
+const Container = styled.div`
+    width: 100%;
+    height: calc(100vh - 160px);
+`;
 
 export default Main;
