d61d2cf8dca0ce7262fa3a59e8bd320adab0180f
MoonHongMyeong <moonhongmyeong@gmail.com>
2023-12-19 16:16:57 +0900

    Add : 리스트 컴포넌트에 등록 버튼 추가 [#3]

--- src/components/list/CharacterList.js
+++ src/components/list/CharacterList.js
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import CharacterItem from './CharacterItem';
+import styled from 'styled-components';
+import { ShortButton } from '../button/Buttons';
 
 const CharacterList = ({characters}) => {
     return (
@@ -7,15 +9,27 @@ const CharacterList = ({characters}) => {
         {   
             Array.from(characters).map((character,index) => {                
                 return (
-                    <>
-                        <CharacterItem key={character.CharacterName} character={character}/>
-                    </>
+                    <CharacterItem key={index} character={character}/>
                 )
             })
+                
         }
+            <RegisterContainer>
+                <ShortButton>등록</ShortButton>
+            </RegisterContainer>
         </>
     )
 
 }
+
+const RegisterContainer = styled.div`
+    width: 80%;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+`;
+
 
 export default CharacterList;
