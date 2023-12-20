9f9877bc1564809f31ddc6df11ab7e1ad74c56b7
MoonHongMyeong <moonhongmyeong@gmail.com>
2023-12-20 17:16:32 +0900

    Add : 체크박스 선택 시 서버에 요청할 리스트의 데이터를 넣고 뺴는 이벤트 구현 [#3]

--- src/components/list/CharacterItem.js
+++ src/components/list/CharacterItem.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Checkbox } from '../input/Inputs';
 
-const CharacterItem = ({character}) => {
+const CharacterItem = ({character, addCharacter, subCharacter}) => {
+
+    const [characterInfo, setCharacterInfo] = useState({
+        characterName: character.CharacterName,
+        characterItemLevel: character.ItemMaxLevel,
+        characterClassName: character.CharacterClassName,
+        characterLevel: character.CharacterLevel,
+        characterServerName: character.ServerName
+    });
+
+    const changeCheckbox = (e) => {
+        if ( e.target.checked ){
+            return addCharacter(characterInfo);
+        }
+        return subCharacter(characterInfo);
+        
+    }
+
     return (
         <Container>
             <CharacterNameContainer>{character.CharacterName}</CharacterNameContainer>
@@ -10,7 +27,7 @@ const CharacterItem = ({character}) => {
             <CharacterClassNameContainer>{character.CharacterClassName}</CharacterClassNameContainer>
             <CharacterLevelContainer>{character.CharacterLevel}</CharacterLevelContainer>
             <CharacterServerNameContainer>{character.ServerName}</CharacterServerNameContainer>
-            <Checkbox type='checkbox' name={character.CharacterName}/>
+            <Checkbox type='checkbox' name={character.CharacterName} onChange={changeCheckbox}/>
         </Container>
     )
 }
--- src/components/list/CharacterList.js
+++ src/components/list/CharacterList.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CharacterItem from './CharacterItem';
 import styled from 'styled-components';
 import { ShortButton } from '../button/Buttons';
+import { SERVER } from '../../assets/server';
+import axios from 'axios';
 
 const CharacterList = ({characters}) => {
+    const [characterList, setCharacterList] = useState([]);
+
+    const registerCharaters = (e) => {
+        console.log(characterList)
+        axios.post(SERVER.REGISTER_CHARACTER_REQUEST, characterList)
+            .then(res => {
+                console.log(res)
+            })
+            .catch(e => console.log(e));
+    }
+
+    const addCharacter = (character) => {
+        setCharacterList(characterList.concat(character));
+    }
+
+    const subCharacter = (character) => {
+        setCharacterList(characterList
+            .filter((inCharacter) => inCharacter.characterName !== character.characterName)
+            );
+    }
+
     return (
         <>
         {   
             Array.from(characters).map((character,index) => {                
                 return (
-                    <CharacterItem key={index} character={character}/>
+                    <CharacterItem key={index} 
+                                   character={character}
+                                   addCharacter={addCharacter}
+                                   subCharacter={subCharacter}
+                    />
                 )
             })
                 
         }
             <RegisterContainer>
-                <ShortButton>등록</ShortButton>
+                <ShortButton className='registerBtn' onClick={registerCharaters}>등록</ShortButton>
             </RegisterContainer>
         </>
     )
